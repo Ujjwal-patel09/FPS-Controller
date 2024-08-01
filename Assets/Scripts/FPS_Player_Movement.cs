4548413dc855f0a19d5a55c87c1c611f93e628a4
Ujjwal-patel09 <ujjupatel009@gmail.com>
2024-08-01 16:46:00 +0530

    adding Crouch movement

--- Assets/Scripts/FPS_Player_Movement.cs
+++ Assets/Scripts/FPS_Player_Movement.cs
@@ -1,95 +1,188 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FPS_Player_Movement : MonoBehaviour
 {
-    
-   [Header("Walking & Running")]
-   public float walking_speed;
-   public float running_speed;
-   public bool Walking;
-   public bool Running;
    private CharacterController characterController;
+
    private Vector3 MoveDirection;
    private Vector3 velocity;
-   private float MoveSpeed; 
+   private float MoveSpeed;
+   
+   // player Input //
+   private float InputX;
+   private float InputZ;
+   private bool Jump_input;
+   private bool Crouch_input;
+
+   [Header("Walking")]
+   [SerializeField] private bool Can_Walk = true;
+   [SerializeField] private float walking_speed;
+   [HideInInspector]public bool iswalking;
+   
+   [Header("Running")]
+   [SerializeField] bool Can_Run = true;
+   [SerializeField] float running_speed;
+   [HideInInspector]public bool isrunning;
     
-   [Header("Jump & Ground_Check")]
-   public float gravity = -10f;
-   public Transform GroundCheck;
-   public float GroundCheck_Sphere_radius = 0.4f;
-   public LayerMask GroundMask;
-   public bool isGrounded;
-   public float JumpHight;
-   public bool Jump;
+   [Header("Ground_Check")]
+   [SerializeField] private float gravity = -10f;
+   [SerializeField] private Transform GroundCheck_object;
+   [SerializeField] private float GroundCheck_Sphere_radius = 0.4f;
+   [SerializeField] private LayerMask GroundMask;
+   [SerializeField] private bool isGrounded;
+
+   [Header("Jumping")]
+   [SerializeField] private  bool canJump = true;
+   [SerializeField] private float JumpHight;
+   [SerializeField] private float Jump_CoolDown_Time;
+   [HideInInspector] public bool isJumping;
+
+   [Header("Crouching")]
+   [SerializeField] private bool CanCrouch = true;
+   [SerializeField] private float crouch_Speed;
+   [SerializeField] private float Stand_hight;
+   [SerializeField] private float Crouch_hight;
+   [SerializeField] private float Time_to_Crouch;
+   [SerializeField] private Vector3 Standing_Center;
+   [SerializeField] private Vector3 Crouching_Center;
+   [SerializeField] private float CrouchRadius;
+   [SerializeField] private float StandRadius;
+
+   [SerializeField] private GameObject camera_holder;
+   [SerializeField] private Vector3 Stand_Cam_Pos;
+   [SerializeField] private Vector3 Crouch_cam_pos;
+   [HideInInspector] public bool isCrouching;
     
    
    void Start()
    {
       characterController = GetComponent<CharacterController>();
+      camera_holder = GameObject.FindGameObjectWithTag("camera_holder");
    }
 
    void Update()
    {
+      PlayerInput();
       movement();
-      Jump_GroundCheck();
+      GroundCheck();
+      Jump();
+      Crouching();
    }
 
-   public void movement()
+   private void PlayerInput()
    {
-      float x = Input.GetAxis("Horizontal");
-      float z = Input.GetAxis("Vertical");
-        
-      // for move in direction of  camera facing//
-      MoveDirection = transform.right * x + transform.forward * z;
-      characterController.Move(MoveDirection * MoveSpeed * Time.deltaTime);
-      
-      // for walking //
-      if(MoveDirection.magnitude > 0.1f)
-      {
-         Walking = true;
-         MoveSpeed = walking_speed;
-      }else{
-         Walking = false;
-      }
-
-      // for Running //
-      if(MoveDirection.magnitude > 0.1f && Input.GetKey(KeyCode.LeftShift))
-      {
-         Running = true;
-         MoveSpeed = running_speed;
-      }else{
-         Running = false;
-      }
-
+      InputX = Input.GetAxis("Horizontal");
+      InputZ = Input.GetAxis("Vertical");
+      Jump_input = Input.GetButtonDown("Jump");
+      Crouch_input = Input.GetKeyDown(KeyCode.LeftControl);
    }
 
-   void Jump_GroundCheck()
+   private void movement()
+   {    
+      // for move in direction of  camera facing//
+      MoveDirection = transform.right * InputX + transform.forward * InputZ;
+      characterController.Move(MoveDirection.normalized * MoveSpeed * Time.deltaTime);
+
+      //  for walking //
+      if(Can_Walk)
+      {
+         if(MoveDirection.magnitude > 0.1f || MoveDirection.magnitude < -0.1f)
+         {
+            MoveSpeed = walking_speed;
+            iswalking = true;  
+         }else
+         {
+            iswalking = false;
+         }
+      }
+      
+      // for Running //
+      if(Can_Run)
+      {
+         if(MoveDirection.magnitude > 0.1f && Input.GetKey(KeyCode.LeftShift))
+         {
+            isrunning = true;
+            MoveSpeed = running_speed;
+         }else
+         {
+            isrunning = false;
+         }
+      }
+   }
+
+   private void GroundCheck()
    {
       // for fall in ground using gravity and velocity //
       velocity.y += gravity * Time.deltaTime;
       characterController.Move(velocity * Time.deltaTime);
 
       // for reset the velocity when player on ground //
-      isGrounded = Physics.CheckSphere(GroundCheck.position,GroundCheck_Sphere_radius,GroundMask);
+      isGrounded = Physics.CheckSphere(GroundCheck_object.position,GroundCheck_Sphere_radius,GroundMask);
       if(isGrounded && velocity.y < 0)
       {
          velocity.y = -2f;
       }
+   }
 
-      // for Jump //
-      if(Input.GetButtonDown("Jump") && isGrounded)
+   private void Jump()
+   {
+      if(canJump)
       {
-         StartCoroutine(jumping());
+         if(Jump_input && isGrounded)
+         {
+            isJumping = true;
+            velocity.y = Mathf.Sqrt(JumpHight * -2f * gravity);// Physics Formula // 
+            StartCoroutine(jumping_CoolDown());
+         }
+   
+         IEnumerator jumping_CoolDown()
+         {
+            yield return new WaitForSeconds(Jump_CoolDown_Time);
+            isJumping = false;
+         }
       }
-      IEnumerator jumping()
+   }
+   
+   private void Crouching()
+   {
+      if(CanCrouch)
       {
-         Jump = true;
-         velocity.y = Mathf.Sqrt(JumpHight * -2f * gravity);
-         yield return new WaitForSeconds(0.7f);
-         Jump = false;
+         if(Crouch_input && isGrounded)
+         {
+            isCrouching = !isCrouching;
+            if(isCrouching){MoveSpeed = crouch_Speed;}
+            StartCoroutine(Crouch_Stand());
+         }
       }
+   }
+
+   IEnumerator Crouch_Stand()
+   {
+      float timeTaken = 0;
+      float TargetHight = isCrouching? Crouch_hight : Stand_hight;
+      float CurrentHight = characterController.height;
+      Vector3 TargetCenter = isCrouching? Crouching_Center : Standing_Center;
+      Vector3 CurrentCenter = characterController.center;
+      float TargetRadius = isCrouching? CrouchRadius : StandRadius;
+      float CurrentRadius = characterController.radius;
+      Vector3 Target_Cam_Pos = isCrouching? Crouch_cam_pos : Stand_Cam_Pos;
+      Vector3 Current_Cam_Pos = camera_holder.transform.localPosition;
+      while(timeTaken < Time_to_Crouch)
+      {
+         characterController.height = Mathf.Lerp(CurrentHight,TargetHight,timeTaken/Time_to_Crouch);
+         characterController.center = Vector3.Lerp(CurrentCenter,TargetCenter,timeTaken/Time_to_Crouch);
+         characterController.radius = Mathf.Lerp(CurrentRadius,TargetRadius,timeTaken/Time_to_Crouch);
+         camera_holder.transform.localPosition = Vector3.Lerp(Current_Cam_Pos,Target_Cam_Pos,timeTaken/Time_to_Crouch);
+         timeTaken += Time.deltaTime;
+         yield return null;
+      }
+
+      characterController.height = TargetHight;
+      characterController.center = TargetCenter;
+      characterController.radius = TargetRadius;
+      camera_holder.transform.localPosition = Target_Cam_Pos;
+
    }
 
 }
--- Assets/Scripts/Player_Animation.cs
+++ Assets/Scripts/Player_Animation.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class Player_Animation : MonoBehaviour
@@ -16,32 +15,110 @@ public class Player_Animation : MonoBehaviour
   {
     transform.localPosition = new Vector3(0,0,0);
 
-    // for walking animation //
-    if(fPS_Player_Movement.Walking)
-    {
-        playerAnimator.SetBool("isWalking",true);
-    }else
-    {
-        playerAnimator.SetBool("isWalking",false);
-    }
-
-    // for running animation //
-    if(fPS_Player_Movement.Running)
-    {
-        playerAnimator.SetBool("isRunning",true);    
-    }else
-    {
-        playerAnimator.SetBool("isRunning",false);
-    }
-        
-    // for jumping animation //
-    if(fPS_Player_Movement.Jump)
-    {
-        playerAnimator.SetBool("isJump",true);
-    }else
-    {
-        playerAnimator.SetBool("isJump",false);
-    }
-        
+    // setting All the float value in blend tree 1 = Walking , 2 = Running //
+    walking_Running_Animation();
+    Jump_Animation();
+    Crouch_Walking_Animation();
+  
   }
+
+  private void walking_Running_Animation()
+  {
+    if(fPS_Player_Movement.iswalking == true)
+    {
+      if(Input.GetKey(KeyCode.W))// for walking & running Front //
+      {
+        if(fPS_Player_Movement.isrunning == true)
+        {
+          playerAnimator.SetFloat("yWalk_Direction",2f,0.1f,Time.deltaTime);
+        }else
+        {
+          playerAnimator.SetFloat("yWalk_Direction",1f,0.1f,Time.deltaTime);
+        }
+      }
+
+      if(Input.GetKey(KeyCode.S))// for walking & running Backward //
+      {
+        if(fPS_Player_Movement.isrunning == true)
+        {
+          playerAnimator.SetFloat("yWalk_Direction",-2f,0.1f,Time.deltaTime);
+        }else
+        {
+          playerAnimator.SetFloat("yWalk_Direction",-1f,0.1f,Time.deltaTime);
+        }
+      }
+
+      if(Input.GetKey(KeyCode.A))// for walking & running Left //
+      {
+        if(fPS_Player_Movement.isrunning == true)
+        {
+          playerAnimator.SetFloat("xWalk_Direction",2f,0.1f,Time.deltaTime);
+        }else
+        {
+          playerAnimator.SetFloat("xWalk_Direction",1f,0.1f,Time.deltaTime);
+        }
+      }
+
+      if(Input.GetKey(KeyCode.D))// for walking & running Right //
+      {
+        if(fPS_Player_Movement.isrunning == true)
+        {
+          playerAnimator.SetFloat("xWalk_Direction",-2f,0.1f,Time.deltaTime);
+        }else
+        {
+          playerAnimator.SetFloat("xWalk_Direction",-1f,0.1f,Time.deltaTime);
+        }
+      }
+    }else
+    {
+      // stay Idle //
+      playerAnimator.SetFloat("yWalk_Direction",0,0.1f,Time.deltaTime);
+      playerAnimator.SetFloat("xWalk_Direction",0,0.1f,Time.deltaTime);
+    }
+  }
+
+  private void Jump_Animation()
+  {
+    if(fPS_Player_Movement.isJumping == true)
+    {
+      playerAnimator.SetBool("Jumping",true);
+    }else
+    {
+      playerAnimator.SetBool("Jumping",false);
+    }
+  }
+
+  private void Crouch_Walking_Animation()
+  {
+    if(fPS_Player_Movement.isCrouching)
+    {
+      playerAnimator.SetBool("isCrouching",true);
+      if(Input.GetKey(KeyCode.W))
+      {
+        playerAnimator.SetFloat("yCrouch_Direction",1,0.1f,Time.deltaTime);
+      }
+      else if(Input.GetKey(KeyCode.S))
+      {
+        playerAnimator.SetFloat("yCrouch_Direction",-1,0.1f,Time.deltaTime);
+      }
+      else if(Input.GetKey(KeyCode.A))
+      {
+        playerAnimator.SetFloat("xCrouch_Direction",1,0.1f,Time.deltaTime);
+      }
+      else if(Input.GetKey(KeyCode.D))
+      {
+        playerAnimator.SetFloat("xCrouch_Direction",-1,0.1f,Time.deltaTime);
+      }
+      else
+      {
+        playerAnimator.SetFloat("xCrouch_Direction",0,0.1f,Time.deltaTime);
+        playerAnimator.SetFloat("yCrouch_Direction",0,0.1f,Time.deltaTime);
+      }
+    }
+    else
+    {
+      playerAnimator.SetBool("isCrouching",false);
+    }
+  }
+
 }
